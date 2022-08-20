ebbeb61beec8dd9a4dd99b6ec9737fe5150a237c
Sumit-dey <deysumit417@gmail.com>
2022-08-19 23:42:07 -0400

    aa

--- SQL/recyclable.sql
+++ SQL/recyclable.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select user_id, concat(upper(substr(name,1,1)),lower(substr(name,2,length(name)))) as name
-from users
-order by user_id
+SELECT product_id
+FROM Products
+WHERE low_fats='Y' AND recyclable='Y'
